b4218c6c1d597520aa8511e9e4d5c866dc5738be
zestx5 <buyankov.ilya@gmail.com>
2023-10-23 19:40:51 +0300

    add: create form controller view and route

---
+++ controllers/note-create.php
@@ -0,0 +1,10 @@
+<?php
+
+$heading = 'Create Note';
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    dd($_POST);
+}
+
+require 'views/note-create.view.php';
--- router.php
+++ router.php
@@ -1,13 +1,7 @@
 <?php
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/notes' => 'controllers/notes.php',
-    '/note' => 'controllers/note.php',
-    '/contact' => 'controllers/contact.php'
-];
+$routes = require 'routes.php';
 
 function routeToController($uri, $routes)
 {
