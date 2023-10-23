215020ca40dd016987e73cbbfb63523b13dfd2cd
zestx5 <buyankov.ilya@gmail.com>
2023-10-23 14:51:19 +0300

    chore: remove variable in config

--- config.php
+++ config.php
@@ -1,8 +1,10 @@
 <?php
 
-return $config = [
-    'host' => 'localhost',
-    'port' => 3306,
-    'dbname' => 'myapp',
-    'charset' => 'utf8mb4'
+return [
+    'database' => [
+        'host' => 'localhost',
+        'port' => 3306,
+        'dbname' => 'myapp',
+        'charset' => 'utf8mb4'
+    ]
 ];
--- index.php
+++ index.php
@@ -6,6 +6,6 @@ require 'Database.php';
 
 $config = require 'config.php';
 
-$db = new Database($config, 'zest', '123456');
+$db = new Database($config['database'], 'zest', '123456');
 
 $posts = $db->query('select * from posts where id > 1')->fetch();
