e318706b9d42a2ee493c157dbeab4b260c42f765
zestx5 <buyankov.ilya@gmail.com>
2023-10-24 12:16:12 +0300

    refactor: notes into recourses

--- controllers/notes/index.php
+++ controllers/notes/index.php
@@ -6,4 +6,4 @@ $db = new Database($config['database'], 'zest', '123456');
 $heading = 'My Notes';
 $notes = $db->query('select * from notes where user_id = ?', [5])->findAll();
 
-require 'views/notes.view.php';
+require 'views/notes/index.view.php';
--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -10,4 +10,4 @@ $note = $db->query('select * from notes where id = ?', [$_GET['id']])->findOrFai
 
 authorize($note['user_id'] === $currentUserId);
 
-require 'views/note.view.php';
+require 'views/notes/show.view.php';
